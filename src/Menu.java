41c0457af0834508a8f32a2fe722f78cd4fd1769
Jitty-04 <jittysjohn1997@gmail.com>
2022-11-21 12:55:08 +0530

    p

---
+++ src/Menu.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+    public class Menu {
+        public static void main(String[] arg) {
+            while (true) {
+                int a,b,c;
+                Scanner sc=new Scanner(System.in);
+                System.out.println("1:Largest among 3 numbers");
+                System.out.println("2:Sallest among 3 numbers");
+                System.out.println("3:Check prime or not");
+                System.out.println("4:Even or not");
+                System.out.println("5:Reverse of a number");
+                System.out.println("6:Exit");
+                c=sc.nextInt();
+
+
+
+            }
+        }
+    }
+
